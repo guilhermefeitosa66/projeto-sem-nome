d8af09562bcb27f116bc8d0a06f6331b8f97874c
Guilherme <guilhermefeitosa66@gmail.com>
2015-05-06 20:31:32 -0300

    created controller abilities with CRUD medthods and your routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 ProjetoSemNome::Application.routes.draw do
   # get '/users' => 'users#index'
+
+  #routes for the ability module
+  get '/abilities' => 'abilities#index', as: 'abilities'
+  get '/abilities/new' => 'abilities#new', as: 'new_ability'
+  get '/abilities/edit/:id' => 'abilities#edit', as: 'edit_ability'
+  get '/abilities/show/:id' => 'abilities#show', as: 'show_ability'
+  post '/abilities/create' => 'abilities#create', as: 'create_ability'
+  post '/abilities/update/:id' => 'abilities#update', as: 'update_ability'
+  get '/abilities/destroy/:id' => 'abilities#destroy', as: 'destroy_ability'
 end
