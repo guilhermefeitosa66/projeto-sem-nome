646ea18b5b7366b158069ea65feda38440d7a7c8
MirlaSantana <mirlasantanaf@gmail.com>
2015-05-06 21:41:05 -0300

    created the views of the language module CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ ProjetoSemNome::Application.routes.draw do
  get '/language/new' => 'languages#new', as: 'new_language'
  post'/language/create' => 'languages#create', as: 'create_language'
  get'/language/edit/:id' => 'languages#edit', as: 'edit_language'
- get'/language/destroy/:id' => 'language#destroy', as: 'destroy_language'
+ get'/language/destroy/:id' => 'languages#destroy', as: 'destroy_language'
  get'/language/show/:id' => 'languages#show', as: 'show_language'
  post'/language/update/:id' => 'languages#update', as: 'update_language'
 
